1ebcf5fee7552f71bf4d1b9569c35b0a5c4c0734
Geremia <geremia.pompei@studenti.unicam.it>
2022-03-29 17:24:50 +0200

    fix extension

--- extension/script.js
+++ extension/script.js
@@ -1,11 +1,16 @@
-
-const address = "0xb5CBC70Eb4CD330adB823e9687F6883EcaC3607F";
-
 const data = {
+  blockchains: [
+    {
+      path: chrome.runtime.getURL("icons/polygon.svg"),
+      contract_url: "https://mumbai.polygonscan.com/address/0xd3fBcedEf11004cC1c0aF64e54f4ca268CA0769C",
+    },
+    {
+      path: chrome.runtime.getURL("icons/ethereum.svg"),
+      contract_url: "https://ropsten.etherscan.io/address/0xb5CBC70Eb4CD330adB823e9687F6883EcaC3607F",
+    },
+  ],
   button_label: "CERTIFICAZIONE",
   button_url: "https://dev-digital-cv.herokuapp.com/",
-  ethereum_path: chrome.runtime.getURL("icons/ethereum.svg"),
-  ethereum_contract_url: "https://ropsten.etherscan.io/address/" + address,
 };
 
 const navbar = document.getElementsByClassName("rd-navbar-nav")[0];
@@ -18,15 +23,16 @@ anchor_button.href = data.button_url;
 li.appendChild(anchor_button);
 navbar.append(li);
 
-const footer =
-  document.getElementsByClassName("portletEmbed")[0].childNodes[1]
-    .childNodes[0];
-const anchor_icon = document.createElement("a");
-anchor_icon.classList.add("fab");
-const img = document.createElement("img");
-img.src = data.ethereum_path;
-img.style.paddingLeft = 0;
-anchor_icon.href = data.ethereum_contract_url;
-anchor_icon.target = "_blank";
-anchor_icon.appendChild(img);
-footer.appendChild(anchor_icon);
+const footer = document.getElementsByClassName("portletEmbed")[0].childNodes[1].childNodes[0];
+for(const blockchain of data.blockchains) {
+  const anchor_icon = document.createElement("a");
+  anchor_icon.classList.add("fab");
+  const img = document.createElement("img");
+  img.src = blockchain.path;
+  img.width = 26;
+  img.style.paddingLeft = 0;
+  anchor_icon.href = blockchain.contract_url;
+  anchor_icon.target = "_blank";
+  anchor_icon.appendChild(img);
+  footer.appendChild(anchor_icon);
+}
