e036de98088ea859f73f6a7b43af1cf010d8cf3e
imdev <info@imdev.it>
2022-03-29 15:53:45 +0200

    add footer style

--- client/public/components/utility/footer.js
+++ client/public/components/utility/footer.js
@@ -1,203 +1,225 @@
 export default {
     template: /*html*/`
+       <section id="5xmille" class="bg-orange cinquexmille"><div class="container"><div class="col-xs-12 col-md-4 main-section">
+        Dona il tuo <strong>5 per mille</strong><br>
+        all’Università di Macerata
+      </div><div class="col-xs-12 col-md-4">
+        Destiniamo le risorse derivanti dal 5 x mille<br>
+        al potenziamento dei programmi di ricerca<br>
+        per sostenere i nostri migliori talenti
+      </div><div class="col-xs-12 col-md-4"><strong>Come fare</strong> Apponi la tua firma nel riquadro<br>
+        "Finanziamento della ricerca scientifica e della<br>
+        Università" con questo codice fiscale<br>
+        00177050432
+      </div></div>
+      <div class="clear"></div>
+      </section>
     <footer id="portal-footer-wrapper" class="footer bg-gray-light" role="contentinfo">
-        <div class="container" id="portal-footer"><div class="row"><div class="col-xs-12"><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a64616c2d31323930" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a64616c2d31323930"><div class="portletStaticText portlet-static-dal-1290"><p><a href="https://www.unimc.it/it/ricerca/hrs4r/hrs4r-human-resources-strategy-for-researchers" title="hrs4r - human resources strategy for researchers"><img alt="HR Excellence in research logo" src="https://www.unimc.it/index_files/dal1290_HR.png" class="image-inline"></a></p></div></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d6c656674" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d6c656674"><aside class="portlet portletNavigationTree"><nav class="portletContent lastItem"><ul class="navTree navTreeLevel0"><li class="navTreeItem visualNoMarker section-accessibilita"><a href="https://www.unimc.it/portal/unimc/resolveuid/9d1951ec275b4d5d90ebcabf2a33dc14" title="" class="state-published ">
+        <div class="container" id="portal-footer">
+        <div class="row"><div class="col-xs-12">
+        <div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a64616c2d31323930" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a64616c2d31323930">
+        <div class="portletStaticText portlet-static-dal-1290"><p>
+        <a href="#" title="hrs4r - human resources strategy for researchers">
+        <img alt="HR Excellence in research logo" src="images/dal1290_HR.png" class="image-inline"></a></p>
+        </div></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d6c656674" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d6c656674"><aside class="portlet portletNavigationTree"><nav class="portletContent lastItem"><ul class="navTree navTreeLevel0"><li class="navTreeItem visualNoMarker section-accessibilita"><a href="https://www.unimc.it/portal/unimc/resolveuid/9d1951ec275b4d5d90ebcabf2a33dc14" title="" class="state-published ">
 
              
 
             
 
             Accessibilità
-        </a></li><li class="navTreeItem visualNoMarker section-dichiarazione-di-accessibilita"><a href="https://form.agid.gov.it/view/5ddacc11-ceb5-42c0-a8b3-269633fa92d3" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-dichiarazione-di-accessibilita"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Dichiarazione di accessibilità
-        </a></li><li class="navTreeItem visualNoMarker section-assistenza-feedback"><a href="https://www.unimc.it/it/unimc-comunica/assistenza-e-feedback" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-assistenza-feedback"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Assistenza e Feedback
-        </a></li><li class="navTreeItem visualNoMarker section-avvertenze-anti-phishing"><a href="http://csia.unimc.it/it/PTA/phishing" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-avvertenze-anti-phishing"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Avvertenze anti-phishing
-        </a></li><li class="navTreeItem visualNoMarker section-credits"><a href="https://www.unimc.it/it/credits" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-credits"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Credits
-        </a></li><li class="navTreeItem visualNoMarker section-dati-monitoraggio-statistiche"><a href="https://www.unimc.it/it/unimc-comunica/dati-monitoraggio-e-statistiche" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-dati-monitoraggio-statistiche"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Dati monitoraggio e statistiche
-        </a></li><li class="navTreeItem visualNoMarker section-guida-al-portale"><a href="https://www.unimc.it/portal/unimc/resolveuid/74ee22ac5cda4f28b5786133383b36e0" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-guida-al-portale"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Guida al Portale
-        </a></li><li class="navTreeItem visualNoMarker section-mappa-del-sito"><a href="https://www.unimc.it/it/sitemap" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-mappa-del-sito"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Mappa del sito
-        </a></li><li class="navTreeItem visualNoMarker section-note-legali"><a href="https://www.unimc.it/it/note-legali" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-note-legali"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Note legali
-        </a></li><li class="navTreeItem visualNoMarker section-privacy"><a href="https://www.unimc.it/it/privacy-policy" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-privacy"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Privacy
-        </a></li><li class="navTreeItem visualNoMarker section-rss-feed"><a href="https://www.unimc.it/it/unimc-comunica/news/RSS" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-rss-feed"><a href="#" title="" class="state-published ">
 
              
 
             
 
             RSS feed
-        </a></li></ul></nav></aside></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d63656e746572" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d63656e746572"><aside class="portlet portletNavigationTree"><nav class="portletContent lastItem"><ul class="navTree navTreeLevel0"><li class="navTreeItem visualNoMarker section-albo-ufficiale"><a href="http://www.unimc.it/albo_online" title="" class="state-published ">
+        </a></li></ul></nav></aside></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d63656e746572" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d63656e746572"><aside class="portlet portletNavigationTree"><nav class="portletContent lastItem">
+        <ul class="navTree navTreeLevel0"><li class="navTreeItem visualNoMarker section-albo-ufficiale"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Albo ufficiale
-        </a></li><li class="navTreeItem visualNoMarker section-amministrazione-trasparente"><a href="https://www.unimc.it/it/quickinfo/amministrazione-trasparente" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-amministrazione-trasparente"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Amministrazione trasparente
-        </a></li><li class="navTreeItem visualNoMarker section-amministrazione-digitale"><a href="https://www.unimc.it/it/amministrazionedigitale/" title="Amministrazione digitale" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-amministrazione-digitale"><a href="#" title="Amministrazione digitale" class="state-published ">
 
              
 
             
 
             Amministrazione digitale
-        </a></li><li class="navTreeItem visualNoMarker section-atti-di-notifica"><a href="https://www.unimc.it/it/ateneo/direttore-generale/ufficio-legale/atti-di-notifica" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-atti-di-notifica"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Atti di notifica
-        </a></li><li class="navTreeItem visualNoMarker section-bandi-e-concorsi"><a href="https://www.unimc.it/it/ateneo/bandi-e-concorsi" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-bandi-e-concorsi"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Bandi e concorsi
-        </a></li><li class="navTreeItem visualNoMarker section-dove-siamo"><a href="https://www.unimc.it/it/unimc-sul-territorio" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-dove-siamo"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Dove siamo
-        </a></li><li class="navTreeItem visualNoMarker section-segnalazioni-e-reclami"><a href="https://www.unimc.it/it/qualita/segnalazioni-e-reclami" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-segnalazioni-e-reclami"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Segnalazioni e reclami
-        </a></li><li class="navTreeItem visualNoMarker section-urp"><a href="https://www.unimc.it/it/ateneo/amministrazione/area-affari-generali/urp" title="L&#39;Ufficio Relazioni con il Pubblico" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-urp"><a href="#" title="L&#39;Ufficio Relazioni con il Pubblico" class="state-published ">
 
              
 
             
 
             URP
-        </a></li></ul></nav></aside></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d7269676874" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d7269676874"><aside class="portlet portletNavigationTree"><nav class="portletContent lastItem"><ul class="navTree navTreeLevel0"><li class="navTreeItem visualNoMarker section-contatti"><a href="https://www.unimc.it/it/unimc-comunica/contatti-e-feedback" title="contatti" class="state-published ">
+        </a></li></ul></nav></aside></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d7269676874" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a666f6f7465722d6e61762d7269676874"><aside class="portlet portletNavigationTree"><nav class="portletContent lastItem">
+        <ul class="navTree navTreeLevel0"><li class="navTreeItem visualNoMarker section-contatti"><a href="#" title="contatti" class="state-published ">
 
              
 
             
 
             Contatti
-        </a></li><li class="navTreeItem visualNoMarker section-servizio-studenti-con-disabilita-e-dsa"><a href="https://www.unimc.it/it/servizi-agli-studenti/servizio-disabilita-e-dsa" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-servizio-studenti-con-disabilita-e-dsa"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Disabilità e DSA
-        </a></li><li class="navTreeItem visualNoMarker section-e-learning"><a href="http://elearning.unimc.it/" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-e-learning"><a href="#" title="" class="state-published ">
 
              
 
             
 
             E-learning
-        </a></li><li class="navTreeItem visualNoMarker section-email-studenti"><a href="https://login.unimc.it/login/new/login-sp/login-sp-15-posta-elettronica-studenti.php?applicationid=15" title="Email studenti" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-email-studenti"><a href="#" title="Email studenti" class="state-published ">
 
              
 
             
 
             E-mail studenti
-        </a></li><li class="navTreeItem visualNoMarker section-faq-come-fare-per"><a href="https://www.unimc.it/it/didattica/faq" title="Le risposte alle domande poste più frequentemente" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-faq-come-fare-per"><a href="#" title="Le risposte alle domande poste più frequentemente" class="state-published ">
 
              
 
             
 
             FAQ. Come fare per
-        </a></li><li class="navTreeItem visualNoMarker section-fundraising"><a href="https://www.unimc.it/it/unimc-comunica/campagna-5x1000" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-fundraising"><a href="#" title="" class="state-published ">
 
              
 
             
 
             Fundraising
-        </a></li><li class="navTreeItem visualNoMarker section-immatricolazione-online"><a href="https://studenti.unimc.it/auth/Login.do" title="link al sistema Esse3" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-immatricolazione-online"><a href="#" title="link al sistema Esse3" class="state-published ">
 
              
 
             
 
             Immatricolazione e esami online
-        </a></li><li class="navTreeItem visualNoMarker section-pagamenti-online"><a href="https://unimc.pagoatenei.cineca.it/portalepagamenti.server.frontend/#/" title="Versamento tasse online" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-pagamenti-online"><a href="#" title="Versamento tasse online" class="state-published ">
 
              
 
             
 
             Paga online
-        </a></li><li class="navTreeItem visualNoMarker section-sportello-spin-off"><a href="https://www.unimc.it/it/lavoro-territorio/terza-missione/spin-off" title="sportello spin off" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-sportello-spin-off"><a href="#" title="sportello spin off" class="state-published ">
 
              
 
             
 
             Sportello Spin off
-        </a></li><li class="navTreeItem visualNoMarker section-wifi-unimc"><a href="http://csia.unimc.it/it/site-news/accesso-wi-fi" title="" class="state-published ">
+        </a></li><li class="navTreeItem visualNoMarker section-wifi-unimc"><a href="#" title="" class="state-published ">
 
              
 
@@ -205,7 +227,10 @@ export default {
 
             WiFi UniMC
         </a></li></ul></nav></aside></div><div class="portletWrapper" id="portletwrapper-706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a616464726573732d616e642d736f6369616c" data-portlethash="706c6f6e652e666f6f746572706f72746c6574730a636f6e746578740a2f706f7274616c2f756e696d632f69740a616464726573732d616e642d736f6369616c"><div class="portletEmbed portlet-embed-address-and-social"><div><address>UNIVERSITÀ DI MACERATA 2018 © <br>Via Crescimbeni, 30/32 - 62100 Macerata</address>CHIAMA UNIMC 
-<ul><li> (+39) 0733 2581</li><li>(+39) 0733 258 2812</li><li>(+39) 0733 258 2813</li></ul>Numero Verde 800 22 40 71<br>PEC ateneo@pec.unimc.it <br><br>Partita IVA: 00177050432</div><div><p><a href="https://www.facebook.com/universita.macerata/" class="fab fa-facebook-f"><span class="sr-only">Facebook</span></a><a href="https://twitter.com/unimc" class="fab fa-twitter"><span class="sr-only">Twitter</span></a><a href="https://www.instagram.com/unimc/" class="fab fa-instagram"><span class="sr-only">Instagram</span></a><a href="https://www.youtube.com/unimcwebtv" class="fab fa-youtube"><span class="sr-only">YouTube</span></a><a href="https://www.linkedin.com/school/universita-di-macerata" class="fab fa-linkedin"><span class="sr-only">LinkedIn</span></a><a class="fab" href="https://ropsten.etherscan.io/address/0xb5CBC70Eb4CD330adB823e9687F6883EcaC3607F" target="_blank"><img src="chrome-extension://glbacmlkefbefmajgbfgolapgkacmaeg/icons/ethereum.svg" style="padding-left: 0px;"></a></p></div></div></div></div></div></div>
+<ul><li> (+39) 0733 2581</li><li>(+39) 0733 258 2812</li><li>(+39) 0733 258 2813</li></ul>Numero Verde 800 22 40 71<br>PEC ateneo@pec.unimc.it <br><br>Partita IVA: 00177050432</div><div>
+
+<img src="images/social.png" class="social_footer_icon"/>
+ </div></div></div></div></div></div>
       </footer>
     `
 }
